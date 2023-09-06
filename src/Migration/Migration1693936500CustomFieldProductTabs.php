332abb7b40706557ec32e29c49d573e3f8ee2432
Sebastian Schreier <info@sebastianschreier.de>
2023-09-06 19:30:59 +0200

    established the compatibility with Shopware from version 6.5

---
+++ src/Migration/Migration1693936500CustomFieldProductTabs.php
@@ -0,0 +1,135 @@
+<?php
+declare(strict_types=1);
+
+namespace Sschreier\TabProductDetailPage\Migration;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Migration\InheritanceUpdaterTrait;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\System\CustomField\CustomFieldTypes;
+use Sschreier\TabProductDetailPage\SschreierTabProductDetailPage;
+
+final class Migration1693936500CustomFieldProductTabs extends MigrationStep
+{
+    use InheritanceUpdaterTrait;
+
+    public const CUSTOM_FIELD_SET_TECHNICAL_NAME = 'sschreier_tabs_';
+
+    public function getCreationTimestamp(): int
+    {
+        return 1693936500;
+    }
+
+    /**
+     * @throws Exception
+     * @throws \Doctrine\DBAL\Driver\Exception
+     */
+    public function update(Connection $connection): void
+    {
+        for ($i = 1; $i <= SschreierTabProductDetailPage::NUMBER_TABS; ++$i) {
+            $customFieldSetId = $this->createCustomFieldSet($connection, $i);
+
+            $this->createCustomField($connection, $customFieldSetId, $i);
+
+            $this->createCustomFieldRelation($connection, $customFieldSetId);
+        }
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+
+    protected function createCustomFieldSet(Connection $connection, int $number): string
+    {
+        $customFieldSetId = Uuid::randomBytes();
+
+        $customFieldSetStmt = $connection->prepare(self::getCustomFieldSetSql());
+        $customFieldSetStmt->executeStatement([
+            'id' => $customFieldSetId,
+            'name' => self::CUSTOM_FIELD_SET_TECHNICAL_NAME . $number,
+            'position' => $number,
+            'config' => '{"label": {"de-DE": "Tab ' . $number .'", "en-GB": "tab ' . $number .'"}}',
+            'created_at' => self::getDateTimeValue(),
+        ]);
+
+        return $customFieldSetId;
+    }
+
+    protected function createCustomField(Connection $connection, string $customFieldSetId, int $number): void
+    {
+        $customFieldId = Uuid::randomBytes();
+
+        $customFieldHeadlineStmt = $connection->prepare(self::getCustomFieldSql());
+        $customFieldHeadlineStmt->executeStatement(
+            [
+                'id' => $customFieldId,
+                'name' => self::CUSTOM_FIELD_SET_TECHNICAL_NAME . 'tab' . $number . '_headline',
+                'fieldType' => CustomFieldTypes::TEXT,
+                'config' => '{"label": {"de-DE": "Überschrift", "en-GB": "headline"}, "placeholder": {"de-DE": "Überschrift des Tabs", "en-GB": "headline of the tab"}, "componentName": "sw-field", "customFieldType": "text", "customFieldPosition": 1, "type": "text"}',
+                'set_id' => $customFieldSetId,
+                'created_at' => self::getDateTimeValue(),
+            ],
+        );
+
+        $customFieldId = Uuid::randomBytes();
+
+        $customFieldContentStmt = $connection->prepare(self::getCustomFieldSql());
+        $customFieldContentStmt->executeStatement(
+            [
+                'id' => $customFieldId,
+                'name' => self::CUSTOM_FIELD_SET_TECHNICAL_NAME . 'tab' . $number . '_content',
+                'fieldType' => CustomFieldTypes::HTML,
+                'config' => '{"label": {"de-DE": "Inhalt", "en-GB": "content"}, "componentName": "sw-text-editor", "customFieldType": "textEditor", "customFieldPosition": 2}',
+                'set_id' => $customFieldSetId,
+                'created_at' => self::getDateTimeValue(),
+            ],
+        );
+    }
+
+    protected function createCustomFieldRelation(Connection $connection, $customFieldSetId): void
+    {
+        $customFieldRelationId = Uuid::randomBytes();
+
+        $customFieldRelationStmt = $connection->prepare(self::getCustomFieldRelationSql());
+        $customFieldRelationStmt->executeStatement(
+            [
+                'id' => $customFieldRelationId,
+                'set_id' => $customFieldSetId,
+                'created_at' => self::getDateTimeValue(),
+                'entity_name' => 'product'
+            ],
+        );
+    }
+
+    protected static function getCustomFieldSetSql(): string
+    {
+        return <<<'SQL'
+            INSERT INTO `custom_field_set` (`id`, `name`, `config`, `active`, `app_id`, `position`, `global`, `created_at`, `updated_at`) VALUES
+            (:id, :name, :config, 1, NULL, :position, 0, :created_at, NULL);
+            SQL;
+    }
+
+    public static function getCustomFieldSql(): string
+    {
+        return <<<'SQL'
+                INSERT INTO `custom_field` (`id`, `name`, `type`, `config`, `active`, `set_id`, `created_at`, `updated_at`, `allow_customer_write`) VALUES
+                (:id, :name, :fieldType, :config, 1, :set_id, :created_at, NULL, 1);
+            SQL;
+    }
+
+    public static function getCustomFieldRelationSql(): string
+    {
+        return <<<'SQL'
+                INSERT INTO `custom_field_set_relation` (`id`, `set_id`, `entity_name`, `created_at`, `updated_at`) VALUES
+                (:id, :set_id, :entity_name, :created_at, NULL);
+            SQL;
+    }
+
+    protected static function getDateTimeValue(): string
+    {
+        return (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT);
+    }
+}
--- src/SschreierTabProductDetailPage.php
+++ src/SschreierTabProductDetailPage.php
@@ -3,130 +3,49 @@ declare(strict_types=1);
 
 namespace Sschreier\TabProductDetailPage;
 
-    use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-    use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-    use Shopware\Core\Framework\Plugin;
-    use Shopware\Core\Framework\Plugin\Context\ActivateContext;
-    use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
-    use Shopware\Core\Framework\Plugin\Context\InstallContext;
-    use Shopware\Core\Framework\Plugin\Context\UninstallContext;
-    use Shopware\Core\Framework\Plugin\Context\UpdateContext;
-    use Shopware\Core\System\CustomField\CustomFieldTypes;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
-    class SschreierTabProductDetailPage extends Plugin
+class SschreierTabProductDetailPage extends Plugin
+{
+    const CUSTOM_FIELD_SET_TECHNICAL_NAME = 'sschreier_tabs_';
+    const NUMBER_TABS = 5;
+
+    public function install(InstallContext $installContext): void
     {
-        const CUSTOM_FIELD_SET_TECHNICAL_NAME = 'sschreier_tabs_';
-        const NUMBER_TABS = 5;
+        parent::install($installContext);
+    }
 
-        public function install(InstallContext $installContext): void
-        {
-            $customFieldSetRepository = $this->container->get('custom_field_set.repository');
-
-            for ($i = 1; $i <= SschreierTabProductDetailPage::NUMBER_TABS; ++$i) {
-                $criteria = new Criteria();
-
-                $criteria->addFilter(new EqualsFilter('name', self::CUSTOM_FIELD_SET_TECHNICAL_NAME . $i));
-
-                $result = $customFieldSetRepository->searchIds($criteria, $installContext->getContext());
-
-                if (!($result->getTotal() > 0)) {
-                    $customFieldSetRepository->create([[
-                        'name' => self::CUSTOM_FIELD_SET_TECHNICAL_NAME . $i,
-                        'config' => [
-                            'label' => [
-                                'de-DE' => 'Tab ' . $i,
-                                'en-GB' => 'tab ' . $i,
-                            ],
-                        ],
-                        'position' => $i,
-                        'customFields' => [
-                            [
-                                'name' => self::CUSTOM_FIELD_SET_TECHNICAL_NAME . 'tab' . $i . '_headline',
-                                'type' => CustomFieldTypes::TEXT,
-                                'config' => [
-                                    'label' => [
-                                        'de-DE' => 'Überschrift',
-                                        'en-GB' => 'headline',
-                                    ],
-                                    'placeholder' => [
-                                        'de-DE' => 'Überschrift des Tabs',
-                                        'en-GB' => 'headline of the tab',
-                                    ],
-                                    'componentName' => 'sw-field',
-                                    'customFieldType' => 'text',
-                                    'customFieldPosition' => 1,
-                                    'type' => 'text',
-                                ],
-                            ],
-                            [
-                                'name' => self::CUSTOM_FIELD_SET_TECHNICAL_NAME . 'tab' . $i . '_content',
-                                'type' => CustomFieldTypes::HTML,
-                                'config' => [
-                                    'label' => [
-                                        'de-DE' => 'Inhalt',
-                                        'en-GB' => 'content'
-                                    ],
-                                    'componentName' => 'sw-text-editor',
-                                    'customFieldType' => 'textEditor',
-                                    'customFieldPosition' => 2,
-                                ]
-                            ],
-                        ],
-                        'relations' => [
-                            ['entityName' => 'product'],
-                        ],
-                    ]], $installContext->getContext());
-                }
-            }
-
-            parent::install($installContext);
-        }
-
-        public function postInstall(InstallContext $installContext): void
-        {
-            parent::postInstall($installContext);
-        }
-
-        public function update(UpdateContext $updateContext): void
-        {
-        }
-
-        public function postUpdate(UpdateContext $updateContext): void
-        {
-        }
-
-        public function activate(ActivateContext $activateContext): void
-        {
-            parent::activate($activateContext);
-        }
-
-        public function deactivate(DeactivateContext $deactivateContext): void
-        {
-            parent::deactivate($deactivateContext);
-        }
-
-        public function uninstall(UninstallContext $uninstallContext): void
-        {
-            if ($uninstallContext->keepUserData()) {
-                parent::uninstall($uninstallContext);
-
-                return;
-            }
-
-            $customFieldSetRepository = $this->container->get('custom_field_set.repository');
-
-            for ($i = 1; $i <= SschreierTabProductDetailPage::NUMBER_TABS; ++$i) {
-                $criteria = new Criteria();
-                $criteria->addFilter(new EqualsFilter('name', self::CUSTOM_FIELD_SET_TECHNICAL_NAME . $i));
-
-                $result = $customFieldSetRepository->searchIds($criteria, $uninstallContext->getContext());
-
-                if ($result->getTotal() > 0 && !$uninstallContext->keepUserData()) {
-                    $data = $result->getDataOfId($result->firstId());
-                    $customFieldSetRepository->delete([$data], $uninstallContext->getContext());
-                }
-            }
-
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if ($uninstallContext->keepUserData()) {
             parent::uninstall($uninstallContext);
+
+            return;
+        }
+
+        $this->removeCustomField($uninstallContext->getContext());
+
+        parent::uninstall($uninstallContext);
+    }
+
+    private function removeCustomField($uninstallContext): void
+    {
+        $customFieldSetRepository = $this->container->get('custom_field_set.repository');
+
+        for ($i = 1; $i <= SschreierTabProductDetailPage::NUMBER_TABS; ++$i) {
+            $criteria = new Criteria();
+            $criteria->addFilter(new EqualsFilter('name', self::CUSTOM_FIELD_SET_TECHNICAL_NAME . $i));
+
+            $result = $customFieldSetRepository->searchIds($criteria, $uninstallContext);
+
+            if ($result->getTotal() > 0) {
+                $data = $result->getDataOfId($result->firstId());
+                $customFieldSetRepository->delete([$data], $uninstallContext);
+            }
         }
     }
+}
